01d36e8ba9d2636a41f39f7dd942e5ed41236e31
Tim Schwartz <tim@metaverse.systems>
2024-07-10 14:42:17 -0500

    chore: Update config.php and Routes.php to use config() function for environment variables

--- src/Routes.php
+++ src/Routes.php
@@ -10,15 +10,15 @@ Route::get('/Description.xml', function() {
 <root xmlns="urn:schemas-upnp-org:device-1-0">
   <device>
     <deviceType>urn:schemas-upnp-org:device:Basic:1</deviceType>
-    <presentationURL><?=env('FRONTEND_URL') ?></presentationURL>
+    <presentationURL><?=config('clarion.frontend_url') ?></presentationURL>
     <friendlyName>Clarion</friendlyName>
     <manufacturer>Metaverse Systems</manufacturer>
     <manufacturerURL>https://metaverse.systems/</manufacturerURL>
     <modelName>Clarion</modelName>
     <modelNumber>0.1</modelNumber>
     <modelURL>https://clarion.app</modelURL>
-    <serialNumber><?=explode('-', env('CLARION_NODE_ID'))[4] ?></serialNumber>
-    <UDN>uuid:<?=env('CLARION_NODE_ID') ?></UDN>
+    <serialNumber><?=explode('-', config('clarion.node_id'))[4] ?></serialNumber>
+    <UDN>uuid:<?=config('clarion.node_id') ?></UDN>
   </device>
 </root>
 <?php
